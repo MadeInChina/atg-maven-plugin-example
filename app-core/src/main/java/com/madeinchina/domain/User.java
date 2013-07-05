b09f2e52e88e2e628473b6f328a77ab4902a20da
MadeInChina <slamhan1987@gmail.com>
2013-07-05 17:48:42 +0800

    add user test.

---
+++ app-core/src/main/java/com/madeinchina/domain/User.java
@@ -0,0 +1,42 @@
+
+package com.madeinchina.domain;
+
+public class User {
+    private String userId;
+    private String userName;
+    private String gender;
+    private String age;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+}
---
+++ app-core/src/test/java/com/madeinchina/domain/UserTest.java
@@ -0,0 +1,22 @@
+
+package com.madeinchina.domain;
+
+import java.io.File;
+
+import atg.test.AtgDustCase;
+
+public class UserTest extends AtgDustCase {
+    public void setUp() throws Exception {
+        copyConfigurationFiles(new String[] {
+                "src/test/resources/config".replace(
+                        "/", File.separator)
+        }, "target/test-classes/config".replace("/",
+                File.separator), ".svn", "CVS");
+    }
+
+    public void test_should_return_user_when_get_from_nucleus() throws Exception {
+        User user = (User) resolveNucleusComponent("/test/User");
+        assertNotNull(user);
+        assertEquals("test", user.getUserId());
+    }
+}
